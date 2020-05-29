1ed6ebd4e6bb4dd9f0000929fcfec4cf163f576d
yousefkhfares <37512258+yousefkhfares@users.noreply.github.com>
2020-05-30 02:23:27 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -25,7 +25,16 @@ if (isset($_SESSION['login'])) {
 					<h2>How It Works</h2>
 				</div>
 			</div>
+				
 			<div class="row fluid">
+			<div class="about-item col-md-4 col-sm-12 col-xs-12">
+					<div class="col-sm User-logo">
+						<img src="/img/landing_page/User.png" alt="User Logo">
+					</div>
+					<i class="fas fa-globe-americas fa-4x"></i>
+					<h3>Create an Account</h3>
+					<p>Create an account and keep track of all your saved data</p>
+				</div>
 				<div class="about-item col-md-4 col-sm-12 col-xs-12">
 					<i class="fas fa-file-image fa-4x"></i>
 					<div class="col-sm location-logo">
@@ -42,14 +51,7 @@ if (isset($_SESSION['login'])) {
 					<h3>Create Tours</h3>
 					<p>Choose from a set of tour types and create new tours that can be edited at any time</p>
 				</div>
-				<div class="about-item col-md-4 col-sm-12 col-xs-12">
-				<div class="col-sm User-logo">
-					<img src="/img/landing_page/User.png" alt="User Logo">
-				</div>
-					<i class="fas fa-globe-americas fa-4x"></i>
-					<h3>Create an Account</h3>
-					<p>Create an account and keep track of all your saved data</p>
-				</div>
+				
 			</div>
 		</div>
 	</div>
@@ -62,7 +64,7 @@ if (isset($_SESSION['login'])) {
 			</div>
 			<div class="row feature-row">
 				<div class="col-sm feature-image">
-					<img src="/img/landing_page/app_engine.svg" alt="Google App Engine Logo">
+					<img src="/img/landing_page/Lock.png" alt="User Authentication Logo">
 				</div>
 				<div class="col-sm feature-detail">
 					<h3>User Authentication</h3>
@@ -77,25 +79,16 @@ if (isset($_SESSION['login'])) {
 					Options to edit existing locations or adding newer locations is always available.</p>
 				</div>
 				<div class="col-sm feature-image">
-					<img src="/img/landing_page/cloud_datastore.svg" alt="Google Cloud Datastore Logo">
+					<img src="/img/landing_page/Location_Services.png" alt="Locations Logo">
 				</div>
 			</div>
 			<div class="row feature-row">
 				<div class="col-sm feature-image">
-					<img src="/img/landing_page/cloud_sql.svg" alt="Google Cloud SQL Logo">
+					<img src="/img/landing_page/Humanoid_Robot.png" alt="Tours Logo">
 				</div>
 				<div class="col-sm feature-detail">
 					<h3>Tours</h3>
 					<p>CHoose from a set of tour types available on our website, and set a tour on the desired location you wish to tour.</p>
-				</div>
-			</div>
-			<div class="row feature-row">
-				<div class="col-sm feature-detail">
-					<h3>Service 4</h3>
-					<p>Lorem ipsum text</p>
-				</div>
-				<div class="col-sm feature-image">
-					<img src="/img/landing_page/cloud_storage.svg" alt="Google Cloud Storage API Logo">
 				</div>
 			</div>
 		</div>
